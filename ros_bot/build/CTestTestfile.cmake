ed943cf2dc315cb77ec33ae5a5b2436f2f1afb37
alekseika7 <alekseykozlov0799@gmail.com>
2020-04-21 18:38:49 +0300

    just workspace

---
+++ ros_bot/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Spring_project/ros_bot/src
+# Build directory: /home/pi/Spring_project/ros_bot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("control_package")
---
+++ ros_bot/build/control_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Spring_project/ros_bot/src/control_package
+# Build directory: /home/pi/Spring_project/ros_bot/build/control_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
