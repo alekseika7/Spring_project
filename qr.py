d339ba5fb36f93470a9c1c9a8bb7fad1377cc481
alekseika7 <alekseykozlov0799@gmail.com>
2020-05-13 01:17:24 +0300

    needs correction

--- qr.py
+++ qr.py
@@ -5,36 +5,45 @@ from pyzbar import pyzbar
 import imutils
 import time
 import cv2
+import numpy as np
 
-# initialize the video stream and allow the camera sensor to warm up
+scale = 2500 # number of pixels in one meter
+work_distance = 0.2 #distance (m) to count angle size of the qr-code
+real_length = 0.04 #real lenght of the qr-code in meters
+real_height = 0.04 #real heigth of the qr-code in meters 
+real_angle_size = 2*np.arctan(real_length/(2*work_distance)) #angle size (rad) of the qr-code on 0.2 m distance 
+
 print("video stream is running....")
 vs = VideoStream(src=0).start()
 time.sleep(2)
 
-# loop over the frames from the video stream
 while True:
     
-    # grab the frame from the threaded video stream and resize it to
-    # have a maximum width of 400 pixels
     frame = vs.read()
     frame = imutils.resize(frame, width=500)
- 
-    # find the barcodes in the frame and decode each of the barcodes
     barcodes = pyzbar.decode(frame)
-    # loop over the detected barcodes
+    
     for barcode in barcodes:
-        # extract the bounding box location of the barcode and draw
-        # the bounding box surrounding the barcode on the image
+        
+        #coordinates and size of qr-code
         (x, y, w, h) = barcode.rect
+        
+        #contour of the qr-code
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
- 
-        # the barcode data is a bytes object so if we want to draw it
-        # on our output image we need to convert it to a string first
+        
+        #counting a distance between camera and qr-code 
+        delta_w = np.abs(frame.shape[1]/2 - (x+w)/2) #center shift only in 'x' coordinate
+        print(h)
+        print(h/scale)
+        distance = h/(scale*real_angle_size) #wrong!!!!! 
+        print(round(distance,3))
+        
+        #converting data to string
         barcodeData = barcode.data.decode("utf-8")
-        #barcodeType = barcode.type
         print(barcodeData)
-            
-            # show the output frame
+        
+          
+    # show the output frame
     cv2.imshow("Barcode Scanner", frame)
     key = cv2.waitKey(1) & 0xFF
  
