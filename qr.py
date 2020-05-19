d9b3c45b6b51c84e5ba35eb09c5369785ce8e919
alekseika7 <alekseykozlov0799@gmail.com>
2020-05-19 23:52:19 +0300

    distance and position

--- qr.py
+++ qr.py
@@ -7,11 +7,13 @@ import time
 import cv2
 import numpy as np
 
-scale = 2500 # number of pixels in one meter
-work_distance = 0.2 #distance (m) to count angle size of the qr-code
-real_length = 0.04 #real lenght of the qr-code in meters
-real_height = 0.04 #real heigth of the qr-code in meters 
-real_angle_size = 2*np.arctan(real_length/(2*work_distance)) #angle size (rad) of the qr-code on 0.2 m distance 
+REAL_WIDTH = 0.05 #real lenght of the qr-code in meters
+REAL_HIGTH = 0.05 #real heigth of the qr-code in meters
+
+#focal length was calculated using real object size (5x5 cm), distance = 10cm
+#and height of the object in pixels (aprox. 200 pixels)
+FOCAL_LENGTH = 400 
+k = 4000 #index for converting from pixels to meters
 
 print("video stream is running....")
 vs = VideoStream(src=0).start()
@@ -20,24 +22,40 @@ time.sleep(2)
 while True:
     
     frame = vs.read()
-    frame = imutils.resize(frame, width=500)
-    barcodes = pyzbar.decode(frame)
+    frame = imutils.resize(frame, 700)
+    xc, yc = int(frame.shape[1]/2), int(frame.shape[0]/2) #center coordinates of the frame
+    cv2.line(frame, (xc, 0), (xc, yc*2), (128, 128, 128), 1)
+    cv2.line(frame, (0, yc), (xc*2, yc), (128, 128, 128), 1)
+    
+    barcodes = pyzbar.decode(frame)  #all qr-codes in the frame
     
     for barcode in barcodes:
         
-        #coordinates and size of qr-code
+        #coordinates and size of the qr-code
         (x, y, w, h) = barcode.rect
         
-        #contour of the qr-code
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        #get coordinates of the qr-code in center axis in cm
+        x0 = round((-xc + x + w/2)*100/k, 3)     
+        y0 = round((yc - y - h/2)*100/k, 3)   
+        z0 = round(FOCAL_LENGTH*REAL_HIGTH*100/h, 3)
         
-        #counting a distance between camera and qr-code 
-        delta_w = np.abs(frame.shape[1]/2 - (x+w)/2) #center shift only in 'x' coordinate
-        print(h)
-        print(h/scale)
-        distance = h/(scale*real_angle_size) #wrong!!!!! 
-        print(round(distance,3))
-        
+        distance = round(np.sqrt((x0/k)**2 + (y0/k)**2 + z0**2), 3)    #distance between the camera and the object in meters
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)    #frame of the qr-code
+        cv2.putText(frame,
+                    f"distance = {distance}",
+                    (x, y-20),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 0, 0),
+                    1)
+        cv2.putText(frame,
+                    f"x={x0}; y={y0}; z={z0}",
+                    (x, y),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 0, 0),
+                    1)
         #converting data to string
         barcodeData = barcode.data.decode("utf-8")
         print(barcodeData)
