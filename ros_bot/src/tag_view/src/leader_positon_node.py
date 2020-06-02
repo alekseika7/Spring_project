75484be47a5b872d304379c16dc8f2ca0a564f5f
alekseika7 <alekseykozlov0799@gmail.com>
2020-06-02 20:14:46 +0300

    simple regulator

--- ros_bot/src/tag_view/src/leader_positon_node.py
+++ ros_bot/src/tag_view/src/leader_positon_node.py
@@ -3,7 +3,8 @@
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from std_msgs.msg import Float32MultiArray
+#from std_msgs.msg import Float32MultiArray, Float32
+from geometry_msgs.msg import Twist
 import cv2
 from pyzbar import pyzbar
 import numpy as np
@@ -11,7 +12,17 @@ import numpy as np
 bridge = CvBridge()
 
 rospy.init_node('get_leader_node')
-pub = rospy.Publisher('/leader_position', Float32MultiArray, queue_size = 1)
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
+
+twist = Twist()
+
+twist.linear.x = 0
+twist.linear.y = 0
+twist.linear.z = 0
+twist.angular.x = 0
+twist.angular.y = 0
+twist.angular.z = 0
+
 
 REAL_WIDTH = 0.05 #real lenght of the qr-code in meters
 REAL_HIGTH = 0.05 #real heigth of the qr-code in meters
@@ -21,32 +32,40 @@ REAL_HIGTH = 0.05 #real heigth of the qr-code in meters
 FOCAL_LENGTH = 400 
 k = 4000 #index for converting from pixels to meters
 
+WORK_DISTANCE = 10  
+
 def get_position(data):
     
     try:
         frame = bridge.imgmsg_to_cv2(data, 'bgr8')
         frame = cv2.resize(frame, (375, 500))
-        xc, yc = int(frame.shape[1]/2), int(frame.shape[0]/2) #center coordinates of the frame
+        xc, yc = frame.shape[1]/2, frame.shape[0]/2 #center coordinates of the frame
         
         barcodes = pyzbar.decode(frame)
-        
-        for barcode in barcodes:
-            #coordinates and size of the qr-code
-            (x, y, w, h) = barcode.rect
+        if len(barcodes) > 0:
+            for barcode in barcodes:
+                #coordinates and size of the qr-code
+                (x, y, w, h) = barcode.rect
+                #get coordinates of the qr-code in center axis in cm
+                x0 = (-xc + x + w/2)*100/k     
+                y0 = (yc - y - h/2)*100/k   
+                z0 = FOCAL_LENGTH*REAL_HIGTH*100/h
             
-            #get coordinates of the qr-code in center axis in cm
-            x0 = round((-xc + x + w/2)*100/k, 3)     
-            y0 = round((yc - y - h/2)*100/k, 3)   
-            z0 = round(FOCAL_LENGTH*REAL_HIGTH*100/h, 3)
+                distance = np.sqrt((x0/k)**2 + (y0/k)**2 + z0**2)    #distance between the camera and the object in meters            
             
-            distance = round(np.sqrt((x0/k)**2 + (y0/k)**2 + z0**2), 3)    #distance between the camera and the object in meters            
-            
-            position = Float32MultiArray()
-            
-            position.data = [distance, x0]
-            
-            pub.publish(position)
-            
+                #position = Float32MultiArray()
+                #position.data = [distance, x0]
+
+                if distance < WORK_DISTANCE:
+                    twist.linear.x = -1
+                elif distance > WORK_DISTANCE:
+                    twist.linear.x = 1
+
+                pub.publish(twist)
+
+        elif len(barcodes) == 0:
+            twist.linear.x = 0
+            pub.publish(twist)
             
     except CvBridgeError as e:
         rospy.loginfo(str(e))
@@ -55,6 +74,7 @@ def get_position(data):
 
 if __name__ =='__main__':
     try:
+        rospy.loginfo("Ready to scan")
         rospy.Subscriber('/cv_camera/image_raw', Image, get_position)
         rospy.spin()
     except rospy.ROSInterruptException:
