6ad54018902344ac78a2f28f34f668a5d4f82170
alekseika7 <alekseykozlov0799@gmail.com>
2020-06-15 19:08:19 +0300

    without cv_camera and cv_bridge

--- ros_bot/src/tag_view/src/leader_positon_node.py
+++ ros_bot/src/tag_view/src/leader_positon_node.py
@@ -1,76 +1,68 @@
 #!/usr/bin/python3.7
 
 import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import Float32MultiArray
-#from geometry_msgs.msg import Twist
+
 import cv2
 from pyzbar import pyzbar
 import numpy as np
 
-bridge = CvBridge()
-position = Float32MultiArray()
+REAL_WIDTH = 0.05   # real lenght of the qr-code in meters
+REAL_HIGTH = 0.05   # real heigth of the qr-code in meters
 
-rospy.init_node('get_leader_node')
-pub = rospy.Publisher('/leader_position', Float32MultiArray, queue_size = 1)
-rate = rospy.Rate(10)
-
-REAL_WIDTH = 0.05 #real lenght of the qr-code in meters
-REAL_HIGTH = 0.05 #real heigth of the qr-code in meters
-
-#focal length was calculated using real object size (5x5 cm), distance = 10cm
-#and height of the object in pixels (aprox. 200 pixels)
+# focal length was calculated using real object size (5x5 cm), distance = 10cm
+# and height of the object in pixels (aprox. 200 pixels)
 FOCAL_LENGTH = 400 
-k = 4000 #index for converting from pixels to meters
+k = 4000    # index for converting from pixels to meters
 
-WORK_DISTANCE = 10  
+'''main fucntion'''
+def scan():
 
-def get_position(data):
+    position = Float32MultiArray()  # array for the distance and angle
+
+    rospy.init_node('get_leader_node')  # initializftion of the node
+    pub = rospy.Publisher('/leader_position', Float32MultiArray, queue_size = 1)    # publisher creation
+
+    rospy.loginfo("Ready to scan")  # command line info 
+
+    cap = cv2.VideoCapture(0)   # get the video stream
     
-    try:
-        frame = bridge.imgmsg_to_cv2(data, 'bgr8')
-        frame = cv2.resize(frame, (375, 500))
-        xc, yc = frame.shape[1]/2, frame.shape[0]/2 #center coordinates of the frame
+    '''working with the video stream'''
+    while not rospy.is_shutdown():
+
+        _, frame = cap.read()   # get the image from the video stream
+
+        frame = cv2.resize(frame, (375, 500))   # resizing 
+        xc, yc = frame.shape[1]/2, frame.shape[0]/2     # center coordinates of the frame
         
-        barcodes = pyzbar.decode(frame)
-        if len(barcodes) > 0:
-            for barcode in barcodes:
-                #coordinates and size of the qr-code
-                (x, y, w, h) = barcode.rect
-                #get coordinates of the qr-code in center axis in cm
-                x0 = (-xc + x + w/2)*100/k     
-                y0 = (yc - y - h/2)*100/k   
-                z0 = FOCAL_LENGTH*REAL_HIGTH*100/h
+        barcodes = pyzbar.decode(frame)     # decoding of the image
+        
+        '''working with qr-code in the image'''
+        if len(barcodes) > 0:   # check if the image includes qr-code
             
-                distance = np.sqrt((x0/k)**2 + (y0/k)**2 + z0**2)    #distance between the camera and the object in meters            
-                angle = np.arctan(x0/distance)
+            for barcode in barcodes:
 
-                position.data = [distance, angle]
+                (x, y, w, h) = barcode.rect     # coordinates and size of the qr-code
+                x0 = (-xc + x + w/2)*100/k     # x coordinate
+                #y0 = (yc - y - h/2)*100/k      # y coordinate 
+                z0 = FOCAL_LENGTH*REAL_HIGTH*100/h      # z coordinate
+            
+                #distance = np.sqrt((x0/k)**2 + (y0/k)**2 + z0**2)    #distance between the camera and the object in meters  
+                distance = np.sqrt((x0/k)**2 + z0**2)   # 2d distance
+                angle = np.arctan(x0/z0)    # angle between the center axis and qr-code
 
-                #if distance < WORK_DISTANCE:
-                    #twist.linear.x = -1
-                #elif distance > WORK_DISTANCE:
-                    #twist.linear.x = 1
+                position.data = [distance, angle]   # final package for publishing 
 
-                pub.publish(position)
+        elif len(barcodes) == 0:    # if no qr-code, will publish constant value
 
-        elif len(barcodes) == 0:
-            #twist.linear.x = 0
             position.data = [-1]
-            pub.publish(position)   
 
-        rate.sleep()
-
-    except CvBridgeError as e:
-        rospy.loginfo(str(e))
+        pub.publish(position)   # publishing the position to the topic /leader_position  
 
 
 
 if __name__ =='__main__':
     try:
-        rospy.loginfo("Ready to scan")
-        rospy.Subscriber('/cv_camera/image_raw', Image, get_position)
-        rospy.spin()
+        scan()
     except rospy.ROSInterruptException:
         pass
