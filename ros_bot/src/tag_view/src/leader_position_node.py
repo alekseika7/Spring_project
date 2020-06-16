ef66ad07b99ef540714a27b4d69da925c3ac58b3
alekseika7 <55062216+alekseika7@users.noreply.github.com>
2020-06-16 15:36:41 +0300

    Faster

--- ros_bot/src/tag_view/src/leader_position_node.py
+++ ros_bot/src/tag_view/src/leader_position_node.py
@@ -1,57 +1,63 @@
 #!/usr/bin/python3.7
 
 import rospy
-from std_msgs.msg import Float32MultiArray
-
 import cv2
+import math
+
+from std_msgs.msg import Int16MultiArray
 from pyzbar import pyzbar
-import numpy as np
 
-REAL_WIDTH = 0.05   # real lenght of the qr-code in meters
-REAL_HIGTH = 0.05   # real heigth of the qr-code in meters
+SCALE = 1/6
 
-# focal length was calculated using real object size (5x5 cm), distance = 10cm
-# and height of the object in pixels (aprox. 200 pixels)
-FOCAL_LENGTH = 400 
-k = 4000    # index for converting from pixels to meters
+QR_H_PIXEL = 1400
+QR_H_MM = 50 #QR heigth of the qr-code in mm
+REF_DISTANCE = 100
 
-'''main fucntion'''
+k = QR_H_PIXEL*SCALE/QR_H_MM #index for converting from pixels to mm
+FOCAL_LENGTH = REF_DISTANCE*k #focal lenght in pixeles
+
+frame_x = int(3280 * SCALE)
+frame_y = int(2464 * SCALE)
+dim = (frame_x, frame_y)
+# center coordinates of the frame
+xc = frame_x/2
+yc = frame_y/2
+
 def scan():
+    
+    position = Int16MultiArray()  # array for the distance and angle
 
-    position = Float32MultiArray()  # array for the distance and angle
+    rospy.init_node('get_leader_node') # initializftion of the node
+    pub = rospy.Publisher('/leader_position', Int16MultiArray, queue_size = 1) # publisher creation
 
-    rospy.init_node('get_leader_node')  # initializftion of the node
-    pub = rospy.Publisher('/leader_position', Float32MultiArray, queue_size = 1)    # publisher creation
-
-    rospy.loginfo("Ready to scan")  # command line info 
-
+    rospy.loginfo("Ready to scan")  # command line info
+    
     cap = cv2.VideoCapture(0)   # get the video stream
     
     '''working with the video stream'''
     while not rospy.is_shutdown():
-
+ 
         _, frame = cap.read()   # get the image from the video stream
 
-        frame = cv2.resize(frame, (375, 500))   # resizing 
-        xc, yc = frame.shape[1]/2, frame.shape[0]/2     # center coordinates of the frame
-        
+        frame = cv2.resize(frame, dim)   #resizing
+
         barcodes = pyzbar.decode(frame)     # decoding of the image
         
         '''working with qr-code in the image'''
         if len(barcodes) > 0:   # check if the image includes qr-code
-            
+
+
             for barcode in barcodes:
 
                 (x, y, w, h) = barcode.rect     # coordinates and size of the qr-code
-                x0 = (-xc + x + w/2)*100/k     # x coordinate
+                x0 = (-xc + x + w/2)/k      # x coordinate
                 #y0 = (yc - y - h/2)*100/k      # y coordinate 
-                z0 = FOCAL_LENGTH*REAL_HIGTH*100/h      # z coordinate
+                z0 = FOCAL_LENGTH*QR_H_MM/h      # z coordinate
             
-                #distance = np.sqrt((x0/k)**2 + (y0/k)**2 + z0**2)    #distance between the camera and the object in meters  
-                distance = np.sqrt((x0/k)**2 + z0**2)   # 2d distance
-                angle = np.arctan(x0/z0)    # angle between the center axis and qr-code
-
-                position.data = [distance, angle]   # final package for publishing 
+                #distance = int(math.sqrt(xp*xp + yp*yp + z0*z0))    #distance between the camera and the object in meters  
+                distance = math.sqrt(x0*x0 + z0*z0)   # 2d distance
+                angle = math.atan(x0/z0)*180/math.pi    # angle between the center axis and qr-code
+                position.data = [int(distance), int(angle)]   # final package for publishing 
 
         elif len(barcodes) == 0:    # if no qr-code, will publish constant value
 
